d7a51e5ce650e72f0395d60c972255a7bbe94228
Sadman Sakib <75657248+ssakibs04@users.noreply.github.com>
2022-09-12 13:57:28 +0600

    Add files via upload

---
+++ Pass_Or_Fail/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int mark;
+    cout<<"Enter Student Mark: :";
+    cin>>mark;
+
+    if(mark>=50){
+          cout<<"You are Pass";
+    }
+
+    else{
+        cout<<"You Are Fail";
+    }
+    getch();
+}
