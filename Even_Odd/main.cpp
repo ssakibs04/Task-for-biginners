592303b2a8385ef93d3b9a25a703470871e998a3
Sadman Sakib <75657248+ssakibs04@users.noreply.github.com>
2022-09-12 13:48:26 +0600

    Add files via upload

---
+++ Even_Odd/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter Any Enteger Number: ";
+    cin>>num;
+
+     if(num%2==0)
+    {
+        cout<<"Even"<<endl;
+
+    }
+
+
+     else
+    {
+        cout<<"Odd";
+    }
+
+    getch();
+}
+
