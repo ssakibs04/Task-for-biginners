feaea8f4ca6bfaa68bb0f0020d44ba857dad862a
Sadman Sakib <75657248+ssakibs04@users.noreply.github.com>
2022-09-12 13:57:05 +0600

    Add files via upload

---
+++ LargeOrSmallBetweenTwoNumbers/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+
+    cout<<"Enter Two Enteger number: "<<endl;
+
+    cin >>num1>>num2;
+
+if(num1>num2){
+
+    cout<<"Large Number is: "<<num1<<endl;
+}
+
+ else {
+    cout<<"Large Number is "<<num2;
+ }
+
+
+      getch();
+
+        }
